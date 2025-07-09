c0cdcdcab5efe461c0934ef28152e0e4872e5768
Tilakraj Desai <33172201+tdTilak@users.noreply.github.com>
2025-07-09 16:25:45 +0530

    Fix import paths in client.go to use module path

--- client.go
+++ client.go
@@ -8,7 +8,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	pb "./hello" // Import the generated protobuf code
+	pb "grpc-hello-world/hello" // Use module path instead of relative import
 )
 
 const (
